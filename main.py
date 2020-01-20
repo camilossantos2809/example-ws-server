9a1f4102298d862c6d9c9290c8b162c30aabc449
camilossantos2809 <camilossantos.2809@gmail.com>
2020-01-20 17:40:57 -0300

    adição de tratamento para novas mensagens

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import json
 from typing import Dict
 
 
-async def ws_handler(websocket, path):
-    host = await websocket.recv()
-    print(f"{host} conectado")
+STATUS = ""
 
-    greeting = f"Client {host} conectado ao WS"
-    await websocket.send(greeting)
+
+async def ws_handler(websocket, path):
+    global STATUS
+    host = await websocket.recv()
+    print(f"msg: {host}", STATUS)
 
     async for message in websocket:
         data: Dict[str, any] = json.loads(message)
@@ -17,16 +18,42 @@ async def ws_handler(websocket, path):
             await websocket.send(json.dumps({
                 "type": "voos",
                 "data": [{
-                    "cod": 1,
-                    "desc": "asdf"
+                    "cod": 4027,
+                    "desc": "AD 4027"
                 }, {
-                    "cod": 2,
-                    "desc": "gfdsa"
+                    "cod": 2500,
+                    "desc": "AD 2500"
                 }]
             }))
+        elif data["type"] == "start_embarque":
+            await asyncio.sleep(5)
+            STATUS = "boarding"
+            await websocket.send(json.dumps({
+                "type": "start_embarque",
+                "data": {"success": True}
+            }))
+        elif data["type"] == "init_embarque":
+            STATUS = "processing"
+            await asyncio.sleep(5)
+            await websocket.send(json.dumps({
+                "type": "init_embarque",
+                "data": {"success": True}
+            }))
+            STATUS = "initiated"
+        elif data["type"] == "end_embarque":
+            await asyncio.sleep(5)
+            STATUS = "not_boarding"
+            await websocket.send(json.dumps({
+                "type": "end_embarque",
+                "data": {"success": True}
+            }))
+        elif data["type"] == "status":
+            await websocket.send(json.dumps({
+                "type": "status",
+                "data": STATUS
+            }))
 
-
-start_server = websockets.serve(ws_handler, "localhost", 3000)
+start_server = websockets.serve(ws_handler, "0.0.0.0", 3000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
